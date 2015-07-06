dded05069bc02bfe202f3fa68a8b18a0220b4356
Will <Will@172.27.35.78>
2015-07-06 15:18:06 -0700

    Age based on DOB function.

--- crash_reporter/public_html/js/crud.js
+++ crash_reporter/public_html/js/crud.js
@@ -4,13 +4,13 @@
  * and open the template in the editor.
  */
 
-
+//Age function
 function getAge(this.dob) {
-                var today = new Date();
-                var birthDate = new Date(this.dob);
-                var age = today.getFullYear() - birthDate.getFullYear();
-                var m = today.getMonth() - birthDate.getMonth();
-                if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-                    age--;
-                }
-                return age;
+    var today = new Date();
+    var birthDate = new Date(this.dob);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
