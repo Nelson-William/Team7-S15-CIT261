6531aa589294a8063215092f7b5c1149583d0743
Will <Will@172.27.35.78>
2015-07-06 14:52:20 -0700

    Age based on DOB function.

--- crash_reporter/public_html/js/crud.js
+++ crash_reporter/public_html/js/crud.js
@@ -5,3 +5,12 @@
  */
 
 
+function getAge(this.dob) {
+                var today = new Date();
+                var birthDate = new Date(this.dob);
+                var age = today.getFullYear() - birthDate.getFullYear();
+                var m = today.getMonth() - birthDate.getMonth();
+                if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                    age--;
+                }
+                return age;
